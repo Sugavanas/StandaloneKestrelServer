b3de61c92edba9b197f60795e40489ed8d396b36
Sugavanas <sugavanas@terminus.studio>
2021-11-10 06:52:47 +0800

    SampleServer

---
+++ tests/SampleServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using StandaloneKestrelServer;
+
+namespace SampleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(configureLogging => configureLogging.SetMinimumLevel(LogLevel.Debug))
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<StandaloneKestrelServerService>();
+                });
+    }
+}
