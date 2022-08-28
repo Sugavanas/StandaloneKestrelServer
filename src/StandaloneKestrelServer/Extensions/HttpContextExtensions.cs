c242710d7ee2a6220aee53236bba53df709da6e3
Sugavanas <sugavanas@terminus.studio>
2022-08-28 14:38:19 +0800

    Obsolete PersistentContainer

--- src/StandaloneKestrelServer/Extensions/HttpContextExtensions.cs
+++ src/StandaloneKestrelServer/Extensions/HttpContextExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace TS.StandaloneKestrelServer.Extensions
@@ -5,6 +6,7 @@ namespace TS.StandaloneKestrelServer.Extensions
     public static class HttpContextExtensions
     {
 #nullable enable
+        [Obsolete("Use HttpContext.Features directly.")]
         public static PersistentContainer? GetPersistentContainer(this HttpContext httpContext)
         {
             return httpContext.Features.Get<PersistentContainer>();
--- src/StandaloneKestrelServer/PersistentContainer.cs
+++ src/StandaloneKestrelServer/PersistentContainer.cs
@@ -9,62 +9,11 @@ namespace TS.StandaloneKestrelServer
     /// Reimplementation of <see cref="Microsoft.AspNetCore.Http.Features.FeatureCollection"/> with some minor changes.
     /// Licensed under MIT License: https://github.com/dotnet/aspnetcore/blob/main/LICENSE.txt
     /// </summary>
-    public class PersistentContainer : IFeatureCollection
+    [Obsolete("Use HttpContext.Features directly.")]
+    public class PersistentContainer : FeatureCollection, IFeatureCollection
     {
-        public bool IsReadOnly => false;
-        
-        public int Revision => 1; //TODO: Maybe implement it? 
-
-        private IDictionary<Type, Object> _container;
-
         public PersistentContainer()
         {
-            _container = new Dictionary<Type, object>();
         }
-
-#nullable enable
-        public object? this[Type key]
-        {
-            get
-            {
-                if (key == null)
-                    throw new ArgumentNullException(nameof(key));
-
-                return (_container.TryGetValue(key, out var result)) ? result : null;
-            }
-            set
-            {
-                if (key == null)
-                    throw new ArgumentNullException(nameof(key));
-
-                if (value == null)
-                {
-                    _container.Remove(key);
-                }
-                else
-                {
-                    _container[key] = value;
-                }
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public IEnumerator<KeyValuePair<Type, object>> GetEnumerator()
-        {
-            return _container.GetEnumerator();
-        }
-
-#pragma warning disable 8766
-        public TFeature? Get<TFeature>() => (TFeature?) this[typeof(TFeature)];
-#pragma warning restore 8766
-
-        public TFeature? Get<TFeature>(out TFeature? result) => result = (TFeature?) this[typeof(TFeature)];
-
-        public void Set<TFeature>(TFeature? instance) => this[typeof(TFeature)] = instance;
-#nullable disable
     }
 }
