4c64ec86944ef3ee4a02fe3d3c157ed56ef7e626
Sugavanas <sugavanas@terminus.studio>
2025-11-01 16:06:41 +0800

    Update SampleServer project to .NET 9.0

--- tests/SampleServer/Program.cs
+++ tests/SampleServer/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,26 @@ namespace SampleServer
         static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            /*
+              // Sample for Minimal API
+
+              var builder = WebApplication.CreateBuilder(args);
+
+              builder.UseStandaloneKestrelServer(options =>
+              {
+                  options.KestrelServerOptions.ListenLocalhost(8050);
+                  options.ConfigureRequestPipeline(ConfigureRequestPipeline);
+              });
+
+              builder.Services.AddRoutingCore();
+
+              var app = builder.Build();
+
+              app.MapGet("/", (HttpContext httpContext) => "Default Kestrel Server");
+
+              app.Run();
+          */
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -28,7 +49,11 @@ namespace SampleServer
 
             builder.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapGet("/",
+                    async context =>
+                    {
+                        await context.Response.WriteAsync("Hello World! - Standalone Kestrel Server");
+                    });
             });
 
             builder.Use(next => async context =>
